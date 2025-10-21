634697aea50c84260385f86014c6cad0e85da311
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2025-10-21 12:53:40 +0200

    Added training code for use with PPO

---
+++ train_ppo.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.path.append("./highway-env/")
+
+import highway_env
+import gymnasium as gym
+from stable_baselines3 import PPO
+
+# Parallel environments
+env = gym.make("merge-single-agent-v0")
+
+model = PPO(
+    "MlpPolicy",
+    env,
+    verbose=1,
+    # n_steps=int(1e6),
+    learning_rate=5e-4,
+    gamma=0.99,
+    vf_coef=0.5,
+    clip_range=0.2,
+    gae_lambda=0.95,
+    n_epochs=10,
+    batch_size=64,
+)
+model.learn(total_timesteps=1e6)
+model.save("ppo_car_following")
