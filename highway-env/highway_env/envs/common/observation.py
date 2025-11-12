0813da3580fa13e994781148ec4ed6e0819cb1df
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2025-11-12 15:59:53 +0100

    Added discretization of target speed

--- highway-env/highway_env/envs/common/observation.py
+++ highway-env/highway_env/envs/common/observation.py
@@ -132,6 +132,11 @@ class RadarObservation(ObservationType):
     def __init__(self, env: "AbstractEnv", **kwargs) -> None:
         super().__init__(env, **kwargs)
 
+        self.vel_limit = 0.68
+        self.vel_resolution = 0.09
+
+        self.vel_bins = np.arange(-self.vel_limit, self.vel_limit, self.vel_resolution)
+
     def space(self) -> spaces.Space:
         return spaces.Box(shape=(4,), low=-1, high=1, dtype=np.float64)
 
@@ -176,7 +181,11 @@ class RadarObservation(ObservationType):
 
         speed = np.dot(v, direction / np.linalg.norm(direction))
 
-        #return self.normalize_obs(np.array([x, y, speed, self.observer_vehicle.speed]))
+        # discretize speed the to match the actual radar outputs
+        ind = np.digitize(speed, self.vel_bins) - 1
+        speed = self.vel_bins[ind]
+
+        # return self.normalize_obs(np.array([x, y, speed, self.observer_vehicle.speed]))
         return np.array([x, y, speed, self.observer_vehicle.speed])
 
 
