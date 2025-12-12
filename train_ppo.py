f426e9e27002d9f88551a0b19353077c5851d7b4
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2025-12-12 14:22:49 +0100

    Structured training script for better use with config file

--- train_ppo.py
+++ train_ppo.py
@@ -15,62 +15,95 @@ from wandb.integration.sb3 import WandbCallback
 
 seed = 42
 
-np.random.seed(seed)
+
 def now_str():
     now = datetime.datetime.now(dateutil.tz.tzlocal())
     return now.strftime("%m-%d:%H-%M:%S.%f")[:-4]
 
-env = gym.make("merge-single-agent-v0")
 
-policy_kwargs = dict(
-    features_extractor_kwargs=dict(features_dim=512),
-)
+def init_seeding(seed):
+    np.random.seed(seed)
 
 
-log_folder = os.path.join(f"logs", now_str())
-os.makedirs(log_folder, exist_ok=True)
-os.system(f"cp -r ./highway-env/ {log_folder}")
+def init_log_folder():
+    log_folder = os.path.join("logs", now_str())
+    os.makedirs(log_folder, exist_ok=True)
+    os.system(f"cp -r ./highway-env/ {log_folder}")
 
-run = wandb.init(
-    project="car-following-env",
-    name=log_folder,
-    #config=config,
-    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    # monitor_gym=True,  # auto-upload the videos of agents playing the game
-    save_code=True,  # optional
-)
+    return log_folder
 
-eval_callback = EvalCallback(env, best_model_save_path=f"{log_folder}/best_model",
-                                     log_path="{log_folder}/results", eval_freq=10000)
 
-wandb_callback=WandbCallback(
-    #gradient_save_freq=100,
-    #model_save_path=f"models/{run.id}",
-    verbose=2
-)
+def init_logging(log_folder):
+    run = wandb.init(
+        project="car-following-env",
+        name=log_folder,
+        # config=config,
+        sync_tensorboard=True,
+        save_code=True,
+    )
 
-callback = CallbackList([eval_callback, wandb_callback])
+    return run
 
-model = PPO(
-    # "MlpPolicy",
-    "CnnPolicy",
-    env,
-    policy_kwargs=policy_kwargs,
-    verbose=1,
-    learning_rate=5e-4,
-    gamma=0.99,
-    vf_coef=0.5,
-    clip_range=0.2,
-    gae_lambda=0.95,
-    n_epochs=10,
-    batch_size=64,
-    tensorboard_log=f"{log_folder}",
-    seed=seed,
-)
 
-model.learn(total_timesteps=1e6,
-            callback=callback
-            )
-model.save(f"{log_folder}/model.zip")
+def init_callbacks(env, log_folder):
+    eval_callback = EvalCallback(
+        env,
+        best_model_save_path=f"{log_folder}/best_model",
+        log_path="{log_folder}/results",
+        eval_freq=10000,
+    )
 
-run.finish()
+    wandb_callback = WandbCallback(
+        # gradient_save_freq=100,
+        # model_save_path=f"models/{run.id}",
+        verbose=2
+    )
+
+    callback = CallbackList([eval_callback, wandb_callback])
+
+    return callback
+
+
+def init_agent(env, log_folder):
+    policy_kwargs = dict(
+        features_extractor_kwargs=dict(features_dim=512),
+    )
+
+    model = PPO(
+        # "MlpPolicy",
+        "CnnPolicy",
+        env,
+        policy_kwargs=policy_kwargs,
+        verbose=1,
+        learning_rate=5e-4,
+        gamma=0.99,
+        vf_coef=0.5,
+        clip_range=0.2,
+        gae_lambda=0.95,
+        n_epochs=10,
+        batch_size=64,
+        tensorboard_log=f"{log_folder}",
+        seed=seed,
+    )
+
+    return model
+
+
+def main():
+    init_seeding(seed)
+    log_folder = init_log_folder()
+    wandb_run = init_logging(log_folder)
+
+    env = gym.make("merge-single-agent-v0")
+
+    model = init_agent(env, log_folder)
+    callback = init_callbacks(env, log_folder)
+
+    model.learn(total_timesteps=1e6, callback=callback)
+    model.save(f"{log_folder}/model.zip")
+
+    wandb_run.finish()
+
+
+if __name__ == "__main__":
+    main()
