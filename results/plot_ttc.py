da5ec22ddcd3440caa342acab7510fc1c61bd083
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2026-02-27 16:27:32 +0100

    Updated plotting scripts to use the CFAR follower

--- results/plot_ttc.py
+++ results/plot_ttc.py
@@ -12,8 +12,13 @@ headway_idm = np.load("./headway_idm_with_action_noise.npy")
 ttc_direct = np.load("./ttc_direct_distance_vel.npy")
 headway_direct = np.load("./headway_direct_distance_vel.npy")
 
-ttc = np.load("./ttc_radar.npy")
-headway = np.load("./headway_radar.npy")
+ttc_cfar = np.load("./ttc_cfar.npy")
+headway_cfar = np.load("./headway_cfar.npy")
+
+# ttc = np.load("./ttc_radar.npy")
+# headway = np.load("./headway_radar.npy")
+ttc = np.load("./ttc_radar_longer_termination_headway_2.npy")
+headway = np.load("./headway_radar_longer_termination_headway_2.npy")
 # ttc = np.load("./ttc_radar_new_pixi.npy")
 # headway = np.load("./headway_radar_new_pixi.npy")
 
@@ -27,6 +32,10 @@ headway_direct = headway_direct[
     headway_direct < 5
 ]  # only use headways below 5 as in paper
 
+ttc_cfar = ttc_cfar[ttc_cfar < 10]  # only use ttcs below 10s as in paper
+ttc_cfar = ttc_cfar[ttc_cfar > 0]  # only use ttcs below 10s as in paper
+headway_cfar = headway_cfar[headway_cfar < 5]  # only use headways below 5 as in paper
+
 ttc = ttc[ttc < 10]  # only use ttcs below 10s as in paper
 ttc = ttc[ttc > 0]  # only use ttcs below 10s as in paper
 headway = headway[headway < 5]  # only use headways below 5 as in paper
@@ -37,6 +46,7 @@ fig, axs = plt.subplots(2, figsize=(20, 10))
 # axs[0].plot(ttc)
 axs[0].hist(ttc_idm, bins=100, density=True, label="IDM")
 axs[0].hist(ttc_direct, bins=100, density=True, label="Direct Input", alpha=0.7)
+axs[0].hist(ttc_cfar, bins=100, density=True, label="CFAR", alpha=0.7)
 axs[0].hist(ttc, bins=100, density=True, label="Range-Doppler Input", alpha=0.7)
 axs[0].set(xlabel="TTC [s]", ylabel="Density")
 axs[0].yaxis.labelpad = 20
@@ -46,6 +56,7 @@ axs[0].legend()
 # axs[1].plot(headway)
 axs[1].hist(headway_idm, bins=100, density=True, label="IDM")
 axs[1].hist(headway_direct, bins=100, density=True, label="Direct Input", alpha=0.7)
+axs[1].hist(headway_cfar, bins=100, density=True, label="CFAR", alpha=0.7)
 axs[1].hist(headway, bins=100, density=True, label="Range-Doppler Input", alpha=0.7)
 axs[1].set(xlabel="Headway [s]", ylabel="Density")
 axs[1].yaxis.labelpad = 20
@@ -54,4 +65,4 @@ axs[1].legend()
 
 plt.subplots_adjust(hspace=0.3)
 # plt.show()
-plt.savefig("ttc_headway_comparison.png", dpi=300, bbox_inches="tight")
+plt.savefig("ttc_headway_comparison_with_cfar.png", dpi=300, bbox_inches="tight")
